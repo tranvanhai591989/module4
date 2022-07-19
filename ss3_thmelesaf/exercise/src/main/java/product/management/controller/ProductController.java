4f0a40d63bc2fdc7bee04dd14bcd6eac8c7c11db
tranvanhai591989 <tranvanhai591989@gmail.com>
2022-07-19 14:29:30 +0700

    asd

--- ss3_thmelesaf/exercise/src/main/java/product/management/controller/ProductController.java
+++ ss3_thmelesaf/exercise/src/main/java/product/management/controller/ProductController.java
@@ -3,10 +3,7 @@ package product.management.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import product.management.model.Product;
 import product.management.service.ProductService;
@@ -54,18 +51,10 @@ public class ProductController {
         return "redirect:/product";
     }
 
-
-    @GetMapping("/{id}/delete")
-    public String delete(@PathVariable int id, Model model) {
-        model.addAttribute("product", productService.findById(id));
-        return "/delete";
-    }
-
-    @PostMapping("/delete")
-    public String delete(Product product, RedirectAttributes redirect) {
-        productService.remove(product.getId());
-        redirect.addFlashAttribute("success", "Delete product " +
-                product.getName() + " successfully!");
+    @GetMapping("/delete")
+    public String delete(@RequestParam int id, RedirectAttributes redirect) {
+        productService.remove(id);
+        redirect.addFlashAttribute("success", "Delete successfully!");
         return "redirect:/product";
     }
 
